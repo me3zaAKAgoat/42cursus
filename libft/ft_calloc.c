b703c45e8cbb253610225c47be8bdac4aeed837e
echoukri <echoukri@me3zasvivobook.localdomain>
2022-10-27 20:57:00 +0100

    updated bzero and calloc to a cleaner version and norminette-ed strlcat

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,27 +6,18 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 12:54:23 by echoukri          #+#    #+#             */
-/*   Updated: 2022/10/21 20:49:54 by echoukri         ###   ########.fr       */
+/*   Updated: 2022/10/27 20:52:33 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* it wouldnt make sense to give malloc a size that is bigger than
-the maximum of size_t and that is SIZE_MAX */
 void	*ft_calloc(size_t count, size_t size)
 {
-	unsigned char	*ptr;
-	unsigned long	index;
+	void	*ptr;
 
 	ptr = malloc(count * size);
-	if (!ptr)
-		return (NULL);
-	index = 0;
-	while (index < count * size)
-	{
-		ptr[index] = 0;
-		index++;
-	}
-	return (ptr);
+	if (ptr)
+		ft_bzero(ptr, count * size);
+	return (NULL);
 }
