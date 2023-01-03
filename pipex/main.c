1ae5a2c1bafa70ac839cf708100d335e68126766
El mehdi Choukri <echoukri@e1r8p3.1337.ma>
2023-01-03 17:17:52 +0100

    figuring out how to do a series of pipes not just 1

--- pipex/main.c
+++ pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 22:47:32 by echoukri          #+#    #+#             */
-/*   Updated: 2023/01/03 16:19:06 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/01/03 17:02:28 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,21 @@ void	first_process(t_pipex_obj *pipex_data,	char *cmd, char *envp[])
 	dup2(pipex_data->infile_d, STDIN_FILENO);
 	dup2(pipex_data->pipe[1], STDOUT_FILENO);
 	close(pipex_data->pipe[0]);
+	cmd_args = ft_split(cmd, ' ');
+	cmd_path = get_cmd(pipex_data->program_paths, cmd_args[0]);
+	execve(cmd_path, cmd_args, envp);
+	split_clear(cmd_args);
+	free(cmd_path);
+	exit(0);
+}
+
+void	mid_process(t_pipex_obj *pipex_data,	char *cmd, char *envp[])
+{
+	char	*cmd_path;
+	char	**cmd_args;
+
+	dup2(pipex_data->pipe[0], STDIN_FILENO);
+	dup2(pipex_data->pipe[1], STDOUT_FILENO);
 	cmd_args = ft_split(cmd, ' ');
 	cmd_path = get_cmd(pipex_data->program_paths, cmd_args[0]);
 	execve(cmd_path, cmd_args, envp);
@@ -60,7 +75,7 @@ int	init(t_pipex_obj *pipex_data, char *argv[], char *envp[])
 	pipex_data->outfile_d = open(argv[4], O_TRUNC | O_CREAT | O_RDWR);
 	if (pipex_data->infile_d == -1 || pipex_data->outfile_d == -1)
 		return (clear_pipex_data(pipex_data), -1);
-	if (pipe(pipex_data->pipe) == -1)
+	if (pipe(pipex_data->interloop_pipe) == -1)
 		return (clear_pipex_data(pipex_data), -1);
 	return (0);
 }
