0e4fd7167ea18e1e2e37de6c7634fdc5c013ecde
El mehdi Choukri <echoukri@e1r6p9.1337.ma>
2023-01-24 13:26:55 +0100

    added some shit to fdf

---
+++ pixel_logic.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pixel_logic.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 10:32:19 by echoukri          #+#    #+#             */
+/*   Updated: 2023/01/24 10:34:46 by echoukri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	my_mlx_pixel_put(t_img_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	if (y > HEIGHT || y < 0)
+		y = 0;
+	if (x > WIDTH || x < 0)
+		x = 0;
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void bresenham(t_img_data *img, point a, point b, int color, int offset_x, int offset_y) {
+	int delta_x = abs(b.x - a.x);
+	int delta_y = abs(b.y - a.y);
+	int sx = a.x < b.x ? 1 : -1;
+	int sy = a.y < b.y ? 1 : -1;
+	int err = delta_x - delta_y;
+
+	while(1) {
+		// draw the current pixel
+		my_mlx_pixel_put(img, offset_x + a.x, offset_y + a.y, color);
+		if (a.x == b.x && a.y == b.y) break;
+		int e2 = 2 * err;
+		if (e2 > -delta_y) {
+			err -= delta_y;
+			a.x += sx;
+		}
+		if (e2 < delta_x) {
+			err += delta_x;
+			a.y += sy;
+		}
+	}
+}
