657f3ccfd294aa5e6232bdc863ebd6b910241508
El mehdi Choukri <echoukri@e1r8p3.1337.ma>
2023-02-24 20:30:53 +0100

    updated some fdf funcitons to conform to normoinette

--- FdF/main.c
+++ FdF/main.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 12:19:10 by echoukri          #+#    #+#             */
-/*   Updated: 2023/02/22 13:04:01 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/02/24 19:01:44 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_point	rotated_point(t_meta_data *fdf, t_point point)
 	double matrix_y[] = {cos(fdf->deg_rota_about_y), 0, sin(fdf->deg_rota_about_y), 0, 1, 0, -sin(fdf->deg_rota_about_y), 0, cos(fdf->deg_rota_about_y)};
 	double matrix_z[] = {cos(fdf->deg_rota_about_z), -sin(fdf->deg_rota_about_z), 0, sin(fdf->deg_rota_about_z), cos(fdf->deg_rota_about_z), 0, 0, 0, 1};
 
-	end_point =	apply_matrix(point, matrix_x);
+	end_point = apply_matrix(point, matrix_x);
 	end_point = apply_matrix(end_point, matrix_y);
 	end_point = apply_matrix(end_point, matrix_z);
 	return (end_point);
@@ -35,34 +35,77 @@ t_point	scaled_point(t_meta_data *fdf, t_point point)
 	return (end_point);
 }
 
+void	draw_horizontal_lines(t_meta_data *fdf, int index)
+{
+	int	next_index;
+
+	next_index = index + 1;
+	if (fdf->points[next_index].x == (fdf->points[index].x + 1))
+		bresenham(fdf,
+			rotated_point(fdf, scaled_point(fdf, fdf->points[index])),
+			rotated_point(fdf, scaled_point(fdf, fdf->points[next_index])));
+}
+
+void	draw_vertical_lines(t_meta_data *fdf, int index)
+{
+	int	next_index;
+
+	next_index = index + fdf->heights[fdf->points[index].y];
+	if (next_index < fdf->nbr_of_points
+		&& (fdf->points[index].x == fdf->points[next_index].x)
+		&& (fdf->points[index].y + 1 == fdf->points[next_index].y))
+		bresenham(fdf,
+			rotated_point(fdf, scaled_point(fdf, fdf->points[index])),
+			rotated_point(fdf, scaled_point(fdf, fdf->points[next_index])));
+}
+
 void	draw_frame(t_meta_data *fdf)
 {
-	int	x_loop_point_index;
-	int	y_loop_point_index;
-	int next_index;
+	int	index;
 
 	mlx_destroy_image(fdf->mlx, fdf->img.img);
 	fdf->img.img = mlx_new_image(fdf->mlx, WIDTH, HEIGHT);
-	y_loop_point_index = 0;
-	while (y_loop_point_index < fdf->nbr_of_points)
+	index = 0;
+	while (index < fdf->nbr_of_points)
 	{
-		if (fdf->points[y_loop_point_index + 1].x == (fdf->points[y_loop_point_index].x + 1))
-			bresenham(fdf,
-				rotated_point(fdf, scaled_point(fdf, fdf->points[y_loop_point_index])),
-				rotated_point(fdf, scaled_point(fdf, fdf->points[y_loop_point_index + 1])));
-		y_loop_point_index++;
+		draw_horizontal_lines(fdf, index);
+		index++;
 	}
-	x_loop_point_index = 0;
-	while (x_loop_point_index < fdf->nbr_of_points)
+	index = 0;
+	while (index < fdf->nbr_of_points)
 	{
-		next_index = fdf->heights[fdf->points[x_loop_point_index].y] + x_loop_point_index;
-		if (next_index < fdf->nbr_of_points && (fdf->points[x_loop_point_index].x == fdf->points[next_index].x) && (fdf->points[x_loop_point_index].y + 1 == fdf->points[next_index].y))
-			bresenham(fdf,
-				rotated_point(fdf, scaled_point(fdf, fdf->points[x_loop_point_index])),
-				rotated_point(fdf, scaled_point(fdf, fdf->points[next_index])));
-		x_loop_point_index++;
+		draw_vertical_lines(fdf, index);
+		index++;
 	}
 	mlx_put_image_to_window(fdf->mlx, fdf->mlx_win, fdf->img.img, 0, 0);
+}
+
+void	handle_directional_key_press(int key, double rota_unit,
+		t_meta_data *fdf)
+{
+	if (key == KEY_LEFT)
+		fdf->deg_rota_about_y -= rota_unit;
+	if (key == KEY_RIGHT)
+		fdf->deg_rota_about_y += rota_unit;
+	if (key == KEY_UP)
+		fdf->deg_rota_about_x += rota_unit;
+	if (key == KEY_DOWN)
+		fdf->deg_rota_about_x -= rota_unit;
+	draw_frame(fdf);
+}
+
+void	handle_translation_key_press(int key, int translation_unit,
+		t_meta_data *fdf)
+{
+	if (key == KEY_W && (fdf->y_translation - translation_unit > -(HEIGHT / 2)))
+		fdf->y_translation -= translation_unit;
+	if (key == KEY_D && (fdf->x_translation + translation_unit <= (WIDTH / 2)))
+		fdf->x_translation += translation_unit;
+	if (key == KEY_S && (fdf->y_translation + translation_unit <= (HEIGHT / 2)))
+		fdf->y_translation += translation_unit;
+	if (key == KEY_A && (fdf->x_translation - translation_unit > -(WIDTH / 2)))
+		fdf->x_translation -= translation_unit;
+	draw_frame(fdf);
 }
 
 int	handle_key_press(int key, t_meta_data *fdf)
@@ -73,52 +116,11 @@ int	handle_key_press(int key, t_meta_data *fdf)
 	rota_unit = 0.03;
 	translation_unit = 30;
 	if (key == KEY_ESC)
-	{
 		exit(0);
-	}
-	if (key == KEY_LEFT)
-	{	
-		fdf->deg_rota_about_y -= rota_unit;
-		draw_frame(fdf);
-	}
-	if (key == KEY_RIGHT)
-	{
-		fdf->deg_rota_about_y += rota_unit;
-		draw_frame(fdf);
-	}
-	if (key == KEY_UP)
-	{	
-		fdf->deg_rota_about_x += rota_unit;
-		draw_frame(fdf);
-	}
-	if (key == KEY_DOWN)
-	{
-		fdf->deg_rota_about_x -= rota_unit;
-		draw_frame(fdf);
-	}
-	if (key == KEY_W && (fdf->y_translation - translation_unit > - ( HEIGHT / 2)))
-	{
-
-		fdf->y_translation -= translation_unit;
-		draw_frame(fdf);
-	}
-	if (key == KEY_D && (fdf->x_translation + translation_unit <= ( WIDTH / 2)))
-	{
-
-		fdf->x_translation += translation_unit;
-		draw_frame(fdf);
-	}
-	if (key == KEY_S && (fdf->y_translation + translation_unit <= ( HEIGHT / 2)))
-	{
-
-		fdf->y_translation += translation_unit;
-		draw_frame(fdf);
-	}
-	if (key == KEY_A && (fdf->x_translation - translation_unit > - ( WIDTH / 2)))
-	{
-		fdf->x_translation -= translation_unit;
-		draw_frame(fdf);	
-	}
+	if (key == KEY_LEFT || key == KEY_RIGHT || key == KEY_UP || key == KEY_DOWN)
+		handle_directional_key_press(key, rota_unit, fdf);
+	if (key == KEY_W || key == KEY_D || key == KEY_S || key == KEY_A)
+		handle_translation_key_press(key, translation_unit, fdf);
 	return (0);
 }
 
@@ -132,13 +134,10 @@ int	handle_zoom(int key, int x, int y, t_meta_data *fdf)
 		if (fdf->scale_factor - unit <= 0)
 			return (0);
 		fdf->scale_factor -= unit;
-		draw_frame(fdf);
 	}
 	else if (key == ZOOM_IN)
-	{	
 		fdf->scale_factor += unit;
-		draw_frame(fdf);
-	}
+	draw_frame(fdf);
 	return (0);
 }
 
--- FdF/pixel_logic.c
+++ FdF/pixel_logic.c
@@ -6,14 +6,14 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 10:32:19 by echoukri          #+#    #+#             */
-/*   Updated: 2023/02/22 18:06:56 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/02/24 19:06:57 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-#define MAX(a,b) (((a) > (b)) ? (a) : (b))
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
+# define MAX(a,b) (((a) > (b)) ? (a) : (b))
+# define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 void	add_pixel_to_frame(t_img_data *data, int x, int y, int color)
 {
@@ -27,41 +27,38 @@ void	add_pixel_to_frame(t_img_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void bresenham(t_meta_data *fdf, t_point a, t_point b)
+void	bresenham(t_meta_data *fdf, t_point a, t_point b)
 {
-    int delta_x = abs(b.x - a.x);
-    int delta_y = abs(b.y - a.y);
-    int sx = a.x < b.x ? 1 : -1;
-    int sy = a.y < b.y ? 1 : -1;
-    int err = delta_x - delta_y;
+	int	delta_x;
+	int	delta_y;
+	int	sx;
+	int	sy;
+	int	err;
+	int	e2;
 
-    // int num_points = MAX(delta_x, delta_y);
-    // int r_diff = (b.color >> 16) & 0xFF - (a.color >> 16) & 0xFF;
-    // int g_diff = (b.color >> 8) & 0xFF - (a.color >> 8) & 0xFF;
-    // int b_diff = (b.color) & 0xFF - (a.color) & 0xFF;
-
-    while (1) {
-        // float t = (float)(MAX(abs(a.x - b.x), abs(a.y - b.y))) / num_points;
-        // int red = (int)(((a.color >> 16) & 0xFF) + r_diff * t);
-        // int green = (int)(((a.color >> 8) & 0xFF) + g_diff * t);
-        // int blue = (int)(((a.color) & 0xFF) + b_diff * t);
-        // int color = (red << 16) | (green << 8) | blue;
-        // color = MAX(color, 0);
-        // color = MIN(color, 0xFFFFFF);
-        add_pixel_to_frame(&fdf->img, a.x + fdf -> x_translation, a.y + fdf-> y_translation, a.color);
-
-        if (a.x == b.x && a.y == b.y) break;
-        int e2 = 2 * err;
-        if (e2 > -delta_y) {
-            err -= delta_y;
-            a.x += sx;
-        }
-        if (e2 < delta_x) {
-            err += delta_x;
-            a.y += sy;
-        }
-    }
+	delta_x = abs(b.x - a.x);
+	delta_y = abs(b.y - a.y);
+	sx = a.x < b.x ? 1 : -1;
+	sy = a.y < b.y ? 1 : -1;
+	err = delta_x - delta_y;
+	while (1)
+	{
+		add_pixel_to_frame(&fdf->img, a.x + fdf -> x_translation, a.y + fdf-> y_translation, a.color);
+		if (a.x == b.x && a.y == b.y) break;
+		e2 = 2 * err;
+		if (e2 > -delta_y)
+		{
+			err -= delta_y;
+			a.x += sx;
+		}
+		if (e2 < delta_x)
+		{
+			err += delta_x;
+			a.y += sy;
+		}
+	}
 }
+
 
 // {
 // 	int delta_x = abs(b.x - a.x);
