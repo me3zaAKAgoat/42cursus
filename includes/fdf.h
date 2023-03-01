125d598c79a1fa331eca6f68900ec57737d8ca4d
El mehdi Choukri <echoukri@e1r8p6.1337.ma>
2023-03-01 20:27:28 +0100

    sepeprate concerns into different modules

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 10:04:26 by echoukri          #+#    #+#             */
-/*   Updated: 2023/02/27 13:43:35 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/03/01 18:42:22 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,28 +24,31 @@
 # define HEIGHT 900
 
 // //macos
-// # define KEY_LEFT		123
-// # define KEY_RIGHT		124
-// # define KEY_DOWN		125
-// # define KEY_UP			126
-// # define KEY_W			13
-// # define KEY_D			2
-// # define KEY_S			1
-// # define KEY_A			0
-// # define KEY_ESC		53
+# define KEY_LEFT		123
+# define KEY_RIGHT		124
+# define KEY_DOWN		125
+# define KEY_UP			126
+# define KEY_W			13
+# define KEY_D			2
+# define KEY_S			1
+# define KEY_A			0
+# define KEY_ESC		53
+# define KEY_ETR		36
+# define KEY_T		17
+# define KEY_Y		16
 // ubuntu
-# define KEY_UP      65362
-# define KEY_RIGHT   65363
-# define KEY_DOWN    65364
-# define KEY_LEFT    65361
-# define KEY_W 122
-# define KEY_D 100
-# define KEY_S 115
-# define KEY_A 113
-# define KEY_ESC 65307
-# define KEY_ETR 65293
-# define KEY_T 116
-# define KEY_Y 121
+// # define KEY_UP      65362
+// # define KEY_RIGHT   65363
+// # define KEY_DOWN    65364
+// # define KEY_LEFT    65361
+// # define KEY_W 122
+// # define KEY_D 100
+// # define KEY_S 115
+// # define KEY_A 113
+// # define KEY_ESC 65307
+// # define KEY_ETR 65293
+// # define KEY_T 116
+// # define KEY_Y 121
 
 //mouse events
 # define ZOOM_IN    4
@@ -87,7 +90,7 @@ extern void		perr_exit(char *msg);
 extern int		count_words(char const *s, char c);
 extern size_t	ft_strlen(const char *s);
 extern t_point	apply_matrix(t_point old_point, double matrix[]);
-extern void		pixel_put_to_img(t_img_data *data, int x, int y, int color);
+extern void		put_pixel_img(t_img_data *data, int x, int y, int color);
 extern void		bresenham(t_meta_data *fdf, t_point a, t_point b);
 extern char		*get_next_line(int fd);
 extern char		*ft_itoa(int n);
@@ -98,5 +101,20 @@ extern char		**ft_split(char const *s, char c);
 extern void		*split_clear(char	**arr);
 extern void		read_map(t_meta_data *fdf, char *file_name);
 extern char		*ft_strchr(const char *s, int c);
+extern void		handle_directional_key_press(int key, double rota_unit,
+					t_meta_data *fdf);
+extern void		handle_translation_key_press(int key, int translation_unit,
+					t_meta_data *fdf);
+extern void		handle_z_key_press(int key, t_meta_data *fdf);
+extern int		handle_key_press(int key, t_meta_data *fdf);
+extern int		handle_zoom(int key, int x, int y, t_meta_data *fdf);
+extern void		draw_horizontal_line(t_meta_data *fdf, int index);
+extern void		draw_vertical_line(t_meta_data *fdf, int index);
+extern void		draw_frame(t_meta_data *fdf);
+extern void		set_base_perspective(t_meta_data *fdf);
+extern t_point	rotated_point(t_meta_data *fdf, t_point point);
+extern t_point	scaled_point(t_meta_data *fdf, t_point point);
+extern void		draw_menu(t_meta_data *fdf);
+
 
 #endif
---
+++ perspective_2.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   perspective_2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 17:45:22 by echoukri          #+#    #+#             */
+/*   Updated: 2023/03/01 18:42:22 by echoukri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	draw_horizontal_line(t_meta_data *fdf, int index)
+{
+	int	next_index;
+
+	next_index = index + 1;
+	if (fdf->points[next_index].x == (fdf->points[index].x + 1))
+		bresenham(fdf,
+			rotated_point(fdf, scaled_point(fdf, fdf->points[index])),
+			rotated_point(fdf, scaled_point(fdf, fdf->points[next_index])));
+}
+
+void	draw_vertical_line(t_meta_data *fdf, int index)
+{
+	int	next_index;
+
+	next_index = index + fdf->y_lengths_arr[fdf->points[index].y];
+	if (next_index < fdf->nbr_of_points
+		&& (fdf->points[index].x == fdf->points[next_index].x)
+		&& (fdf->points[index].y + 1 == fdf->points[next_index].y))
+		bresenham(fdf,
+			rotated_point(fdf, scaled_point(fdf, fdf->points[index])),
+			rotated_point(fdf, scaled_point(fdf, fdf->points[next_index])));
+}
+
+void	draw_frame(t_meta_data *fdf)
+{
+	int	index;
+	
+	mlx_destroy_image(fdf->mlx, fdf->img.img);
+	fdf->img.img = mlx_new_image(fdf->mlx, WIDTH, HEIGHT);
+	index = 0;
+	while (index < fdf->nbr_of_points)
+	{
+		draw_horizontal_line(fdf, index);
+		index++;
+	}
+	index = 0;
+	while (index < fdf->nbr_of_points)
+	{
+		draw_vertical_line(fdf, index);
+		index++;
+	}
+	mlx_put_image_to_window(fdf->mlx, fdf->mlx_win, fdf->img.img, 0, 0);
+	draw_menu(fdf);
+}
