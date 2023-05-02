8fe8121385266d1080fb14d09d6547d86b68d8b9
El mehdi Choukri <echoukri@e1r6p9.1337.ma>
2023-05-02 19:37:21 +0100

    updated sa and sb to more efficient versions

--- src/stack_utils/ra.c
+++ src/stack_utils/ra.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 16:30:01 by echoukri          #+#    #+#             */
-/*   Updated: 2023/05/02 18:55:54 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/05/02 19:08:00 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ void	ra(t_node	**stack_a_p)
 	if (ll_size(*stack_a_p) == 1)
 		return ;
 	else if (ll_size(*stack_a_p) == 2)
+	{
 		sa(stack_a_p);
+		return ;
+	}
 	value_to_bottom = ll_shift(stack_a_p);
 	value_to_top = ll_pop(*stack_a_p);
 	ll_unshift(stack_a_p, ll_new(value_to_top));
--- src/stack_utils/rb.c
+++ src/stack_utils/rb.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 16:36:59 by echoukri          #+#    #+#             */
-/*   Updated: 2023/05/02 18:55:40 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/05/02 19:07:53 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ void	rb(t_node	**stack_b_p)
 	if (ll_size(*stack_b_p) == 1)
 		return ;
 	else if (ll_size(*stack_b_p) == 2)
+	{
 		sb(stack_b_p);
+		return ;
+	}
 	value_to_bottom = ll_shift(stack_b_p);
 	value_to_top = ll_pop(*stack_b_p);
 	ll_unshift(stack_b_p, ll_new(value_to_top));
