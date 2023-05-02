8b7fb90de04b8ebbc3811c4bdbca9c0d1c513f81
El mehdi Choukri <echoukri@e1r6p9.1337.ma>
2023-05-02 15:15:54 +0100

    spelled out the algorithm

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 17:08:35 by echoukri          #+#    #+#             */
-/*   Updated: 2023/05/01 01:21:42 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/05/01 19:59:47 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	del(void *p)
 	free(p);
 }
 
-t_node	*create_linked_list_from_string(char *str)
+t_node	*create_ll_from_string(char *str)
 {
 	t_node	*head;
 	char	**arr;
@@ -26,10 +26,10 @@ t_node	*create_linked_list_from_string(char *str)
 
 	i = 1;
 	arr = ft_split(str, ' ');
-	head = lst_new(ft_atoi(*arr));
+	head = ll_new(ft_atoi(*arr));
 	while (*(arr + i))
 	{
-		lst_push(&head, lst_new(ft_atoi(*(arr + i))));
+		ll_push(&head, ll_new(ft_atoi(*(arr + i))));
 		i++;
 	}
 	split_clear(arr);
@@ -40,12 +40,12 @@ t_node *build_result_list(t_node *seq, t_node *indexes, int max_length, int max_
     t_node *result;
     int 	i;
 	
-	result = lst_fill_int(max_length, 0);
+	result = ll_fill_int(max_length, 0);
 	i = max_index;
     while (i >= 0) {
         max_length--;
-        lst_atindex(result, max_length)->value = lst_value_atindex(seq, i);
-        i = lst_value_atindex(indexes, i);
+        ll_atindex(result, max_length)->value = ll_value_atindex(seq, i);
+        i = ll_value_atindex(indexes, i);
     }
     return (result);
 }
@@ -62,27 +62,27 @@ t_node	*longest_increasing_subsquence(t_node	*seq)
 	t_node	*result;
 	
 	max_length = 0;
-	sequence_length = lst_size(seq);
-	lengths = lst_fill_int(sequence_length, 1);
-	indexes = lst_fill_int(sequence_length, -1);
+	sequence_length = ll_size(seq);
+	lengths = ll_fill_int(sequence_length, 1);
+	indexes = ll_fill_int(sequence_length, -1);
 	i = 0;
 	while (i < sequence_length)
 	{
 		j = 0;
 		while (j < i)
 		{
-			if (lst_value_atindex(seq, j) < lst_value_atindex(seq, i)
-				&& lst_value_atindex(lengths, j) + 1 >  lst_value_atindex(lengths, i))
+			if (ll_value_atindex(seq, j) < ll_value_atindex(seq, i)
+				&& ll_value_atindex(lengths, j) + 1 >  ll_value_atindex(lengths, i))
 			{
-				lst_atindex(lengths, i)->value = lst_value_atindex(lengths, j) + 1;
-				lst_atindex(indexes, i)->value = j;
+				ll_atindex(lengths, i)->value = ll_value_atindex(lengths, j) + 1;
+				ll_atindex(indexes, i)->value = j;
 			}
 			j++;
 		}
 		
-		if (lst_value_atindex(lengths, i) > max_length)
+		if (ll_value_atindex(lengths, i) > max_length)
 		{
-			max_length = lst_value_atindex(lengths, i);
+			max_length = ll_value_atindex(lengths, i);
 			max_index = i;
 		}
 		i++;
@@ -90,8 +90,8 @@ t_node	*longest_increasing_subsquence(t_node	*seq)
 
 	result = build_result_list(seq, indexes, max_length, max_index);
 	
-	lst_clear(&indexes);
-	lst_clear(&lengths);
+	ll_clear(&indexes);
+	ll_clear(&lengths);
 
 	return (result);
 }
@@ -104,7 +104,7 @@ int	main(int ac, char **av)
 
 	if (ac < 2)
 		exit(1);
-	head = create_linked_list_from_string(av[1]);
+	head = create_ll_from_string(av[1]);
 	lis = longest_increasing_subsquence(head);
 	iterator = lis;
 	while (iterator)
@@ -112,7 +112,7 @@ int	main(int ac, char **av)
 		printf("%d ", iterator->value);
 		iterator = iterator->next;
 	}
-	// lst_clear(&head);
+	// ll_clear(&head);
 	// system("leaks push_swap");
 	exit(0);
 }
