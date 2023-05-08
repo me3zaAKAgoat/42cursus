0088d10a45907393496872d6ab5de83995761ef2
El mehdi Choukri <echoukri@e1r8p9.1337.ma>
2023-05-08 21:23:49 +0100

    fixed all norminette issues

--- push_swap/src/sorting/lis.c
+++ push_swap/src/sorting/lis.c
@@ -6,71 +6,91 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 15:24:29 by echoukri          #+#    #+#             */
-/*   Updated: 2023/05/08 19:25:47 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/05/08 21:22:20 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_node *build_result_list(t_node *seq, t_node *indexes,
+t_node	*build_result_list(t_node *seq, t_node *indexes,
 			int max_length, int max_index)
 {
-	t_node *result;
-	int 	i;
-	
+	t_node	*result;
+	int		i;
+
 	result = ll_fill_int(max_length, 0);
 	i = max_index;
-	while (i >= 0) {
+	while (i >= 0)
+	{
 		max_length--;
-		ll_atindex(result, max_length)->value = ll_value_atindex(seq, i);
+		(*ll_atindex(result, max_length)).value = ll_value_atindex(seq, i);
 		i = ll_value_atindex(indexes, i);
 	}
 	return (result);
 }
 
-t_node	*longest_increasing_subsquence(t_node	*seq)
+void	find_lengths_and_indexes(t_node *seq, t_node *lengths, t_node *indexes)
 {
-	int 	i;
-	int 	j;
-	int		sequence_length;
-	int		max_length;
-	int		max_index;
-	t_node	*lengths;
-	t_node	*indexes;
-	t_node	*result;
-	
-	max_length = 0;
-	sequence_length = ll_size(seq);
-	lengths = ll_fill_int(sequence_length, 1);
-	indexes = ll_fill_int(sequence_length, -1);
+	int	i;
+	int	j;
+	int	sequence_length;
+
 	i = 0;
+	sequence_length = ll_size(seq);
 	while (i < sequence_length)
 	{
 		j = 0;
 		while (j < i)
 		{
 			if (ll_value_atindex(seq, j) < ll_value_atindex(seq, i)
-				&& ll_value_atindex(lengths, j) + 1 >  ll_value_atindex(lengths, i))
+				&& ll_value_atindex(lengths, j) + 1
+				> ll_value_atindex(lengths, i))
 			{
-				ll_atindex(lengths, i)->value = ll_value_atindex(lengths, j) + 1;
-				ll_atindex(indexes, i)->value = j;
+				(*ll_atindex(lengths, i)).value
+					= ll_value_atindex(lengths, j) + 1;
+				(*ll_atindex(indexes, i)).value = j;
 			}
 			j++;
 		}
-		
-		if (ll_value_atindex(lengths, i) > max_length)
+		i++;
+	}
+}
+
+void	max_length_and_index(t_node *seq, t_node *lengths,
+	int *max_index_p, int *max_length_p)
+{
+	int	i;
+	int	sequence_length;
+
+	sequence_length = ll_size(seq);
+	(*max_length_p) = 0;
+	i = 0;
+	while (i < sequence_length)
+	{
+		if (ll_value_atindex(lengths, i) > (*max_length_p))
 		{
-			max_length = ll_value_atindex(lengths, i);
-			max_index = i;
+			(*max_length_p) = ll_value_atindex(lengths, i);
+			(*max_index_p) = i;
 		}
 		i++;
 	}
+}
 
+t_node	*longest_increasing_subsquence(t_node	*seq)
+{
+	int		max_length;
+	int		max_index;
+	t_node	*lengths;
+	t_node	*indexes;
+	t_node	*result;
+
+	lengths = ll_fill_int(ll_size(seq), 1);
+	indexes = ll_fill_int(ll_size(seq), -1);
+	find_lengths_and_indexes(seq, lengths, indexes);
+	max_length_and_index(seq, lengths, &max_index, &max_length);
 	result = build_result_list(seq, indexes, max_length, max_index);
-	
 	ll_clear(&indexes);
 	ll_clear(&lengths);
-
 	return (result);
 }
 
