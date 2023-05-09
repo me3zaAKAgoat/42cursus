0ae463a402a104f6141c9699a51c0a651d0c8c50
me3za <medkrix1@gmail.com>
2023-05-09 21:16:27 +0100

    polished up checker

--- src/bonus/checker.c
+++ src/bonus/checker.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 00:53:40 by echoukri          #+#    #+#             */
-/*   Updated: 2023/05/09 20:44:41 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/05/09 21:13:04 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,43 @@
 
 int	execute_instruction(t_stacks *stacks, char *instruction)
 {
-	if (ft_strncmp(instruction, "sa\n", ft_strlen("sa\n")))
+	if (!ft_strncmp(instruction, "sa\n", ft_strlen("sa\n")))
 		return (sa(&stacks->stack_a, 0), 1);
-	else if (ft_strncmp(instruction, "sb\n", ft_strlen("sb\n")))
+	else if (!ft_strncmp(instruction, "sb\n", ft_strlen("sb\n")))
 		return (sb(&stacks->stack_b, 0), 1);
-	else if (ft_strncmp(instruction, "ss\n", ft_strlen("ss\n")))
+	else if (!ft_strncmp(instruction, "ss\n", ft_strlen("ss\n")))
 		return (ss(&stacks->stack_a, &stacks->stack_b, 0), 1);
-	else if (ft_strncmp(instruction, "ra\n", ft_strlen("ra\n")))
+	else if (!ft_strncmp(instruction, "ra\n", ft_strlen("ra\n")))
 		return (ra(&stacks->stack_a, 0), 1);
-	else if (ft_strncmp(instruction, "rb\n", ft_strlen("rb\n")))
+	else if (!ft_strncmp(instruction, "rb\n", ft_strlen("rb\n")))
 		return (rb(&stacks->stack_b, 0), 1);
-	else if (ft_strncmp(instruction, "rr\n", ft_strlen("rr\n")))
+	else if (!ft_strncmp(instruction, "rr\n", ft_strlen("rr\n")))
 		return (rr(&stacks->stack_a, &stacks->stack_b, 0), 1);
-	else if (ft_strncmp(instruction, "rra\n", ft_strlen("rra\n")))
+	else if (!ft_strncmp(instruction, "rra\n", ft_strlen("rra\n")))
 		return (rra(&stacks->stack_a, 0), 1);
-	else if (ft_strncmp(instruction, "rrb\n", ft_strlen("rrb\n")))
+	else if (!ft_strncmp(instruction, "rrb\n", ft_strlen("rrb\n")))
 		return (rrb(&stacks->stack_b, 0), 1);
-	else if (ft_strncmp(instruction, "rrr\n", ft_strlen("rrr\n")))
+	else if (!ft_strncmp(instruction, "rrr\n", ft_strlen("rrr\n")))
 		return (rrr(&stacks->stack_a, &stacks->stack_b, 0), 1);
-	else if (ft_strncmp(instruction, "pa\n", ft_strlen("pa\n")))
+	else if (!ft_strncmp(instruction, "pa\n", ft_strlen("pa\n")))
 		return (pa(&stacks->stack_a, &stacks->stack_b, 0), 1);
-	else if (ft_strncmp(instruction, "pb\n", ft_strlen("pb\n")))
+	else if (!ft_strncmp(instruction, "pb\n", ft_strlen("pb\n")))
 		return (pb(&stacks->stack_a, &stacks->stack_b, 0), 1);
 	else
 		return (0);
+}
+
+void	ll_print(t_node	*head)
+{
+	t_node	*iterator;
+
+	iterator = head;
+	while (iterator)
+	{
+		printf("%d ", iterator->value);
+		iterator = iterator->next;
+	}
+	printf("(null)\n");
 }
 
 void	handle_user_instructions(t_stacks *stacks)
@@ -61,6 +74,7 @@ int	main(int ac, char **av)
 	t_node		*lis;
 
 	stacks.stack_a = parse_input(ac, av);
+	stacks.stack_b = NULL;
 	handle_user_instructions(&stacks);
 	lis = longest_increasing_subsquence(stacks.stack_a);
 	if (ll_size(lis) == ll_size(stacks.stack_a))
